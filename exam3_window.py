bc0eb63b2b4222bb10b7ba632c3b4a8f3936bad3
bum5520 <bum5520@naver.com>
2016-05-12 17:25:44 +0900

    Add exam3_window.py

---
+++ exam3_window.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt4.QtGui import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        """
+        super().__는 부모 클래스의 생성자를 호출할때사용
+        """
+        self.setWindowTitle("Study for stock")
+        self.setGeometry(300,400,300,300)
+        button1 = QPushButton('pycharm',self)
+        button1.move(100,100)
+        button1.clicked.connect(self.button1_clicked)
+
+    def button1_clicked(self):
+        QMessageBox.about(self,"window","clicked")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MainWindow()
+    myWindow.show()
+    app.exec_()
